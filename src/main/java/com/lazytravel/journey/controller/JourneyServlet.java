7b9d8162cf3c2beb1459a4136d373d5a4b051e9a
shan <shan0815332@gmail.com>
2023-11-24 22:54:11 +0800

    路徑改用annotation註冊

--- src/main/java/com/lazytravel/journey/controller/JourneyServlet.java
+++ src/main/java/com/lazytravel/journey/controller/JourneyServlet.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.lazytravel.journey.dao.JourneyService;
 import com.lazytravel.journey.entity.Journey;
 
+@WebServlet(name = "JourneyServlet", value = "/journey/journey.do")
 public class JourneyServlet extends HttpServlet {
 	
 	@Override
